0d468e32fd55a55694ba2c714f74a739e807516d
Ludger Radke <113907719+ludgerradke@users.noreply.github.com>
2023-03-13 08:29:38 +0100

    Update poetry-env-setup39.sh

--- poetry-env-setup39.sh
+++ poetry-env-setup39.sh
@@ -7,6 +7,10 @@ sudo apt-get update; sudo apt-get install make build-essential libssl-dev zlib1g
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
+# Install python and pip
+sudo apt-get install python
+sudo apt-get install pip
+
 # Install Git
 sudo apt install git
 
@@ -29,7 +33,7 @@ pyenv install 3.9.1
 pyenv global 3.9.1
 
 # Install Poetry using curl
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
+curl -sSL https://install.python-poetry.org | python3 -
 
 # Add poetry to the shell path
 echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.bashrc
